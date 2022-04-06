808452096e3c69bf5748b2dec31e121a368b931f
URuo <100269382+URuo@users.noreply.github.com>
2022-04-06 16:49:56 +0800

    Add files via upload

---
+++ qz4/qz4.js
@@ -0,0 +1,9 @@
+function change() { 
+    $(document).ready(function(){
+        $("H1").text("CSIE@CGU");
+       });
+       
+    $(document).ready(function(){
+        $("P").text("怎麼那麼棒！！.");
+    });
+} 
