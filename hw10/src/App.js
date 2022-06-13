5db7608e34faf23b7df03910fdb129f759c8def4
URuo <100269382+URuo@users.noreply.github.com>
2022-06-13 22:23:07 +0800

    Update App.js

--- hw10/src/App.js
+++ hw10/src/App.js
@@ -1,4 +1,10 @@
 import React, { useEffect, useState } from 'react'
+import PeopleIcon from '@mui/icons-material/People';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+import EmailIcon from '@mui/icons-material/Email';
+import HomeIcon from '@mui/icons-material/Home'
+import LinkIcon from '@mui/icons-material/Link'
+
 const divStyle = {
     // border: "1px solid black",
     height: "500px",
@@ -8,11 +14,6 @@ const divStyle = {
 
 function App() {
     const [user, setUser] = useState([])
-    // var url = "https://api.github.com/users/Uruo"
-    // useEffect(() => {
-    //     fetch(url).then(res => res.json().then(res => setUser(res.items)))
-    // }, [])
-    // console.log(user)
 
     useEffect(() => {
         const fetchData = async () => {
@@ -37,27 +38,26 @@ function App() {
                 <button width="150">Edit profile</button>
             </center>
             <br />
-            <svg text="muted" aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-people">
-                <path fill-rule="evenodd" d="M5.5 3.5a2 2 0 100 4 2 2 0 000-4zM2 5.5a3.5 3.5 0 115.898 2.549 5.507 5.507 0 013.034 4.084.75.75 0 11-1.482.235 4.001 4.001 0 00-7.9 0 .75.75 0 01-1.482-.236A5.507 5.507 0 013.102 8.05 3.49 3.49 0 012 5.5zM11 4a.75.75 0 100 1.5 1.5 1.5 0 01.666 2.844.75.75 0 00-.416.672v.352a.75.75 0 00.574.73c1.2.289 2.162 1.2 2.522 2.372a.75.75 0 101.434-.44 5.01 5.01 0 00-2.56-3.012A3 3 0 0011 4z"></path>
-            </svg>
-            <p style={{ display: 'inline-block' }}>{user.followers}followers·{user.following}following</p>
-            <br />
-            {/* <a>{user.public_repos}repos·{user.public_gists}gists</a> */}
-            <svg class="octicon octicon-location" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M11.536 3.464a5 5 0 010 7.072L8 14.07l-3.536-3.535a5 5 0 117.072-7.072v.001zm1.06 8.132a6.5 6.5 0 10-9.192 0l3.535 3.536a1.5 1.5 0 002.122 0l3.535-3.536zM8 9a2 2 0 100-4 2 2 0 000 4z"></path></svg>
-            <p style={{ display: 'inline-block' }}>{user.location}</p>
-            <br />
-            <svg class="octicon octicon-mail" viewBox="0 0 16 16" version="1.1" width="16" height="16" aria-hidden="true"><path fill-rule="evenodd" d="M1.75 2A1.75 1.75 0 000 3.75v.736a.75.75 0 000 .027v7.737C0 13.216.784 14 1.75 14h12.5A1.75 1.75 0 0016 12.25v-8.5A1.75 1.75 0 0014.25 2H1.75zM14.5 4.07v-.32a.25.25 0 00-.25-.25H1.75a.25.25 0 00-.25.25v.32L8 7.88l6.5-3.81zm-13 1.74v6.441c0 .138.112.25.25.25h12.5a.25.25 0 00.25-.25V5.809L8.38 9.397a.75.75 0 01-.76 0L1.5 5.809z"></path></svg>
-            <p style={{ display: 'inline-block' }}>{user.email ? user.email : "無"}</p>
-            <br />
-            <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-link">
-                <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path>
-            </svg>
-            <p style={{ display: 'inline-block' }}>{user.html_url}</p>
-            <br />
-            <svg aria-hidden="true" height="16" viewBox="0 0 16 16" version="1.1" width="16" data-view-component="true" class="octicon octicon-link">
-                <path fill-rule="evenodd" d="M7.775 3.275a.75.75 0 001.06 1.06l1.25-1.25a2 2 0 112.83 2.83l-2.5 2.5a2 2 0 01-2.83 0 .75.75 0 00-1.06 1.06 3.5 3.5 0 004.95 0l2.5-2.5a3.5 3.5 0 00-4.95-4.95l-1.25 1.25zm-4.69 9.64a2 2 0 010-2.83l2.5-2.5a2 2 0 012.83 0 .75.75 0 001.06-1.06 3.5 3.5 0 00-4.95 0l-2.5 2.5a3.5 3.5 0 004.95 4.95l1.25-1.25a.75.75 0 00-1.06-1.06l-1.25 1.25a2 2 0 01-2.83 0z"></path>
-            </svg>
-            <p style={{ display: 'inline-block' }}>{user.blog}</p>
+            <div>
+                <PeopleIcon />
+                <span style={{ display: 'inline-block' }}>{user.followers}followers·{user.following}following</span>
+            </div>
+            <div>
+                <LocationOnIcon />
+                <span style={{ display: 'inline-block' }}>{user.location}</span>
+            </div>
+            <div>
+                <EmailIcon />
+                <span style={{ display: 'inline-block' }}>{user.email ? user.email : "無"}</span>
+            </div>
+            <div>
+                <HomeIcon />
+                <span style={{ display: 'inline-block' }}>{user.html_url}</span>
+            </div>
+            <div>
+                <LinkIcon />
+                <span style={{ display: 'inline-block' }}>{user.blog}</span>
+            </div>
         </div>
     )
 }
